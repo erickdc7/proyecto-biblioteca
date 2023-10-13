46eec232076703be61a9f28b8e792db8de2e60e8
Jose Salirrosas <vegetamygod@gmail.com>
2023-10-12 20:52:22 -0500

    boton register

--- app/src/main/java/com/erickdiaz/proyectobiblioteca/registro_usuario.java
+++ app/src/main/java/com/erickdiaz/proyectobiblioteca/registro_usuario.java
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.content.Intent;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,24 +16,11 @@ import android.view.ViewGroup;
  */
 public class registro_usuario extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment registro_usuario.
-     */
-    // TODO: Rename and change types and number of parameters
     public static registro_usuario newInstance(String param1, String param2) {
         registro_usuario fragment = new registro_usuario();
         Bundle args = new Bundle();
@@ -58,6 +47,22 @@ public class registro_usuario extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.register_activity, container, false);
+        View view = inflater.inflate(R.layout.register_activity, container, false);
+
+        Button btnRegistrar = view.findViewById(R.id.btnRegistrar);
+
+        // Agrega un listener al botón de registro
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Aquí debes agregar el código para navegar a la actividad "MainActivity".
+                // Para hacerlo, puedes utilizar un Intent.
+
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
