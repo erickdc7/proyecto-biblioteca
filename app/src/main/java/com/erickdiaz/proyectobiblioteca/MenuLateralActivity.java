ac5c20fbc3884a5f999e5b68a60717e2ccc1faef
Jose Salirrosas <vegetamygod@gmail.com>
2023-10-16 23:25:15 -0500

    updte

---
+++ app/src/main/java/com/erickdiaz/proyectobiblioteca/MenuLateralActivity.java
@@ -0,0 +1,27 @@
+package com.erickdiaz.proyectobiblioteca;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuLateralActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menuprincipal);
+
+}
+
+    public void irMenuLateral(View v) {
+        Intent reg = new Intent(this, OpcionesActivity.class);
+        startActivity(reg);
+    }
+
+    public void irLogin(View v) {
+        Intent reg = new Intent(this, LoginActivity.class);
+        startActivity(reg);
+    }
+}
